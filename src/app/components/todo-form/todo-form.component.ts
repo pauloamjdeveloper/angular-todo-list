d9b42178c485c08de3e4e24bf53e3478efbb4110
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-09-25 09:32:37 -0300

    Criando modal para adicionar o todo

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -12,6 +12,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { TodoSignalsService } from 'src/app/services/todo-signals.service';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { HeaderComponent } from '../header/header/header.component';
 
 @Component({
   selector: 'app-todo-form',
@@ -24,6 +26,7 @@ import { TodoSignalsService } from 'src/app/services/todo-signals.service';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
+    MatDialogModule
   ],
   templateUrl: './todo-form.component.html',
   styleUrls: [],
@@ -31,6 +34,7 @@ import { TodoSignalsService } from 'src/app/services/todo-signals.service';
 
 export class TodoFormComponent {
   private todoSignalsService = inject(TodoSignalsService);
+  private dialogRefService = inject(MatDialogRef<HeaderComponent>);
   public allTodos = this.todoSignalsService.todosState();
 
   public todosForm = new FormControl({
@@ -46,6 +50,11 @@ export class TodoFormComponent {
       const done = false;
 
       this.todoSignalsService.updateTodos({ id, title, description, done });
+      this.dialogRefService.close();
     }
+  }
+
+  public handleCloseModal(): void {
+    this.dialogRefService.close();
   }
 }
