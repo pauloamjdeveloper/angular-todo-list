17c4a1c284b2574f1e6f5be6fd99eb63567ee36e
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-09-27 21:12:57 -0300

    Aplicando melhorias e adicionando imagens

--- src/app/components/todo-card/todo-card.component.ts
+++ src/app/components/todo-card/todo-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, computed, inject } from '@angular/core';
-import { CommonModule, NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -38,6 +38,7 @@ export class TodoCardComponent implements OnInit {
       this.todosSignal.mutate((todos) => {
         const todoSelected = todos.find((todo) => todo?.id === todoId) as Todo;
         todoSelected && (todoSelected.done = true);
+        this.saveTodosInLocalStorage();
       });
     }
   }
